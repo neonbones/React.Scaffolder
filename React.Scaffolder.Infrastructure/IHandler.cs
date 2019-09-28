dd989a76ff25f2629a155b620cab5121c362713d
Rustam Asylgareev <rustam@smarthead.com>
2019-09-29 01:31:00 +0300

    code cleanup

--- React.Scaffolder.Infrastructure/IHandler.cs
+++ React.Scaffolder.Infrastructure/IHandler.cs
@@ -1,23 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace React.Scaffolder.Infrastructure
+﻿namespace React.Scaffolder.Infrastructure
 {
-    public interface IHandler<in TIn>
-    {
-        void Handle(TIn input);
-    }
-
     public interface IHandler<in TIn, out TOut>
     {
         TOut Handle(TIn input);
-    }
-
-    public interface IAsyncHandler<in TIn, TOut>
-    {
-        Task<TOut> Handle(TIn input, CancellationToken cancellationToken);
     }
 }
